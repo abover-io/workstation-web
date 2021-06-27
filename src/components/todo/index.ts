b2080540a85bfe2694ae4e471cb28f547a167504
Adam Rafiandri <rafiandria23@gmail.com>
2021-06-27 22:58:45 +0700

    TODO-2
    - Created TodoPriorityPicker.

--- src/components/todo/index.ts
+++ src/components/todo/index.ts
@@ -2,3 +2,4 @@ export { default as TodoList } from './todo-list';
 export { default as TodoItem } from './todo-item';
 export { default as AddTodoForm } from './add-todo-form';
 export { default as UpdateTodoForm } from './update-todo-form';
+export { default as TodoPriorityPicker } from './todo-priority-picker';
